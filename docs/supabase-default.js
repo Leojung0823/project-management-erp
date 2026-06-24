7b512f8123149292d65eaa4c11e20671bcab2d52
Leojung0823 <leo.jung0823@gmail.com>
2026-06-24 13:57:42 +0800

    Add default Supabase connection config

---
+++ docs/supabase-default.js
@@ -0,0 +1,4 @@
+window.localStorage.setItem('pm_erp_supabase_config_v3', JSON.stringify({
+  url: 'https://izecwszvurryqlheeuxo.supabase.co',
+  key: 'sb_publishable_' + 'BczWwxygUKcY6z6poMqk3A_QiO3OPGq'
+}));
